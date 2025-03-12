85491412805c98304549bb70e5c9b3cd95c111c4
Omkar Sathe <omkarsathe@himsoft.online>
2025-03-12 17:29:11 +0530

    Refine membership payment receipt label and formatting

--- resources/views/admin/membership/receipt.blade.php
+++ resources/views/admin/membership/receipt.blade.php
@@ -14,7 +14,7 @@
         <div style="width: 300px; height: 30px; border-left: 5px solid white; background-color: #b30d00; position: absolute; top: 0px; right: -20px; transform: skew(40deg);"></div>
         <img src="{{asset('assets/images/logo.png')}}" style="height: 100px; width: 100px; border-radius: 50%; position: absolute; right: 10px; top: 40px" alt="">
         <div style="width: 100%; padding-top: 35px; padding-left: 30px; display: flex; flex-direction: column; color: black">
-            <span style="font-weight: bold; font-size: 30px; color: #B30D00FF; font-family: Roboto">Payment Receipt</span>
+            <span style="font-weight: bold; font-size: 30px; color: #B30D00FF; font-family: Roboto">Membership Payment Receipt</span>
             <span style="font-family: Roboto; font-size: 16px; margin-top: 5px; color: black"> <span style="font-weight: bold; font-size: 20px; color: black">Date:</span> {{\Carbon\Carbon::parse($membership->start_date)->format('d/m/Y')}} </span>
         </div>
         <div style="display: flex;padding-left: 30px; padding-right: 30px; margin-top: 50px; ">
@@ -33,7 +33,7 @@
                 <span style="font-size: 16px;font-family: Roboto;color: black"> <span style="font-size: 18px">£</span> {{$membership->payment_amount}}</span>
             </div>
             <div style="width: 130px;border-bottom: 1px solid transparent;">
-                <span style="font-family: Roboto; font-size: 16px; font-weight: bold;color: black">Membership Type</span>
+                <span style="font-family: Roboto; font-size: 16px; font-weight: bold;color: black">Type</span>
             </div>
             <div style="padding-left: 5px;border-bottom: 1px solid gray; width: 100%">
                 <span style="font-size: 16px;font-family: Roboto; color: black">{{ucfirst($membership->membership_type)}}</span>
